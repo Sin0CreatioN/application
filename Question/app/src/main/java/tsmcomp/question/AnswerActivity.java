3e0df07915c3ec691f8efbd6637f69c2a26014c0
anddev68 <chk392013@gmail.com>
2016-09-14 15:08:50 +0900

    fragment was fixed.

--- Question/app/src/main/java/tsmcomp/question/AnswerActivity.java
+++ Question/app/src/main/java/tsmcomp/question/AnswerActivity.java
@@ -1,31 +1,29 @@
 package tsmcomp.question;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.ListFragment;
 import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
+
 import android.widget.TextView;
 
-import com.nifty.cloud.mb.core.DoneCallback;
-import com.nifty.cloud.mb.core.FindCallback;
-import com.nifty.cloud.mb.core.NCMB;
-import com.nifty.cloud.mb.core.NCMBException;
-import com.nifty.cloud.mb.core.NCMBObject;
-import com.nifty.cloud.mb.core.NCMBQuery;
 
-import java.util.List;
+import com.nifty.cloud.mb.core.NCMB;
+import com.nifty.cloud.mb.core.NCMBObject;
+
 
 import tsmcomp.question.ui.QuestionListFragment;
 
 
 public class AnswerActivity extends AppCompatActivity {
     NCMBObject question;
+
+    private final QuestionListFragment.OnClickTitleListener onClickTitleListener =
+            new QuestionListFragment.OnClickTitleListener() {
+                @Override
+                public void onClickTitle(NCMBObject obj) {
+
+                }
+            };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +33,7 @@ public class AnswerActivity extends AppCompatActivity {
         NCMB.initialize(this.getApplicationContext(), QuestionActivity.KEY1, QuestionActivity.KEY2);
 
         getSupportFragmentManager().beginTransaction()
-                .replace(R.id.answer_layout, new QuestionListFragment(), "list")
+                .replace(R.id.answer_layout, new QuestionListFragment(onClickTitleListener), "list")
                 .commit();
 
     }
--- Question/app/src/main/java/tsmcomp/question/ui/QuestionListFragment.java
+++ Question/app/src/main/java/tsmcomp/question/ui/QuestionListFragment.java
@@ -1,11 +1,18 @@
 package tsmcomp.question.ui;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.nifty.cloud.mb.core.FindCallback;
 import com.nifty.cloud.mb.core.NCMBException;
@@ -22,56 +29,133 @@ import tsmcomp.question.R;
  * クリックされたらコールバックを行う
  * Created by dell_user on 2016/09/14.
  */
-public class QuestionListFragment extends Fragment {
+public class QuestionListFragment extends ListFragment {
 
-    ArrayList<String> questionTitle;
-    List<NCMBObject> ncmbObjects;
-    ArrayAdapter<String> adapter;
-    ListView listView;
+    private static String TAG = "QuestionListFragment";
+    OnClickTitleListener listener;
+
+    public QuestionListFragment(OnClickTitleListener l){
+        this.listener = l;
+    }
 
     @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
-        //  ListViewを初期設定する
-        
-        questionTitle = new ArrayList<>();
-        questionTitle.add("質問がありません");
-        adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, questionTitle);
+    @Override
+    public void onStart(){
+        super.onStart();
 
-
-
-
+        //  ダウンロードを開始する
         NCMBQuery<NCMBObject> query = new NCMBQuery<>("Questions");
         query.whereEqualTo("answerPossible", true);
         query.findInBackground(new FindCallback<NCMBObject>() {
             @Override
             public void done(List<NCMBObject> list, NCMBException e) {
-                if (list.size() == 0) {
-                    title = new String[1];
-                    title[0] = "質問がありません";
-                } else {
-                    ncmbObjects = list;
-                    title = new String[list.size()];
-                    for (int i = 0; i < title.length; i++) {
-                        title[i] = list.get(i).getString("title");
-                    }
-                }
-
-                setListAdapter(adapter);
+                onFind(list, e);
             }
         });
-
+        //  クリックしたときのリスナーをセットしておく
         getListView().setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                question = ncmbObjects.get(position);
-                getSupportFragmentManager().beginTransaction()
-                        .replace(R.id.answer_layout, new AnswerFormFragment(), "form")
-                        .commit();
+            public void onItemClick(AdapterView<?> adapterView, View view, int pos, long id) {
+                //  AdapterにNCMBAdapterをセットしたので、この方法でNCMBObjectを持ってくることができる
+                onClickTitle(adapterView.getAdapter().getItem(pos));
+                //System.out.println( adapterView.getAdapter().getItem(pos).getClass() );
             }
         });
 
     }
+
+
+
+    /**
+     * 質問タイトルをクリックした場合に呼ばれる
+     * @param object クリックされたオブジェクト
+     */
+    private void onClickTitle(Object object){
+        //  NCMBオブジェクトがクリックされたときのみコールバックする
+        if( object instanceof  NCMBObject ){
+            listener.onClickTitle((NCMBObject)object);
+        }
+
+        //NCMBObject question = ncmbObjects.get(position);
+        //listener.onClickTitle(question);
+    }
+
+
+    /**
+     * DBからの更新処理が終わった時に呼ばれる
+     * リストビューを同期する
+     */
+    private void onFind(List<NCMBObject> list, NCMBException e){
+        //  エラーチェック
+        if(e != null){
+            e.printStackTrace();
+            Toast.makeText(getContext(), "予期せぬエラー", Toast.LENGTH_SHORT);
+            return;
+        }
+        if( list.size() == 0 ){
+            Toast.makeText(getContext(), "質問の取得に失敗しました", Toast.LENGTH_SHORT);
+            //  質問なしのアダプターをセットする
+            setListAdapter(new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, new String[]{"現在投稿されている質問がありません"}));
+            return;
+        }
+        //  質問が見つかりました
+        Log.d(TAG,"Finding was done.");
+
+        //ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, NCMBObjectList2StringList(list));
+        //setListAdapter(adapter);
+        setListAdapter(new NCMBObjectArrayAdapter(getContext(), android.R.layout.simple_list_item_1, list));
+    }
+
+
+    /**
+     * フラグメント内の質問タイトルがクリックされた場合は
+     * このフラグメント内で処理するのではなく、Activityで処理させる
+     * そのためリスナーを作成して伝播させる
+     */
+    public interface OnClickTitleListener{
+        void onClickTitle(NCMBObject obj);
+    }
+
+
+    /**
+     * NCMBObjectList -> StringList
+     * このメソッドはこのクラス内だけ使用可
+     * @param objects
+     * @return
+     */
+    private static ArrayList<String> NCMBObjectList2StringList(List<NCMBObject> objects){
+        ArrayList<String> str = new ArrayList<>();
+        for(NCMBObject obj : objects){
+            str.add(obj.getString("title"));
+        }
+        return str;
+    }
+
+
+    /**
+     * このメソッドはこのクラス内だけ使用可
+     * アダプターを拡張して、NCMBObjectをそのまま表示できるようにした
+     */
+    private class NCMBObjectArrayAdapter extends ArrayAdapter<NCMBObject>{
+        public NCMBObjectArrayAdapter(Context context, int resource, List<NCMBObject> objects) {
+            super(context, resource, objects);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            TextView textView = (TextView)View.inflate(getContext(), android.R.layout.simple_list_item_1, null);
+            textView.setText(getItem(position).getString("title"));
+            return textView;
+            //return super.getView(position, convertView, parent);
+        }
+
+
+    }
+
+
 
 }
