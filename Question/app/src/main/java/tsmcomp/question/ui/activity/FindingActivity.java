0bbe629c9a9fc886573d6e9367eb7dab76c7da18
anddev68 <chk392013@gmail.com>
2016-09-25 18:06:07 +0900

    finding activity was implemented.

--- Question/app/src/main/java/tsmcomp/question/ui/activity/FindingActivity.java
+++ Question/app/src/main/java/tsmcomp/question/ui/activity/FindingActivity.java
@@ -1,7 +1,9 @@
 package tsmcomp.question.ui.activity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.Layout;
 import android.view.LayoutInflater;
@@ -26,17 +28,22 @@ public class FindingActivity extends AppCompatActivity{
     @Override
     public void onCreate(Bundle bundle){
         super.onCreate(bundle);
+        setContentView(R.layout.activity_finding);
+
 
         //  アンケート一覧を取得
         questions = new ArrayList<>();
         questions.add(new NCMBQuestion("好きな食べ物は？"));
         questions.add(new NCMBQuestion("好きなお菓子は？"));
         questions.add(new NCMBQuestion("好きな芸能人は？"));
+        questions.add(new NCMBQuestion("好きな〇×は？"));
 
 
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         MyAdapter myAdapter = new MyAdapter();
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(myAdapter);
+
     }
 
 
@@ -59,15 +66,22 @@ public class FindingActivity extends AppCompatActivity{
         @Override
         public void onBindViewHolder(MaterialCardAvatarWithTextViewHolder holder, int position) {
             //  ここで値をセットする
+            //  とりあえずランダムでセット
             NCMBQuestion question = questions.get(position);
             holder.mPrimaryTextView.setText(question.getTitle());
-            holder.mSecondaryTextView.setText("text");
-            holder.mAvatarImageView.setImageResource(R.drawable.add_circle);
+            if( position%2==0 ){
+                holder.mSecondaryTextView.setText("2016/09/09 12:00まで回答できます");
+                holder.mAvatarImageView.setImageResource(R.drawable.account_circle);
+            }else{
+                holder.mSecondaryTextView.setText("2016/09/09 12:00に回答しました");
+                holder.mAvatarImageView.setBackgroundColor(Color.GREEN);
+                holder.mAvatarImageView.setImageResource(R.drawable.check_circle);
+            }
         }
 
         @Override
         public int getItemCount() {
-            return 0;
+            return questions.size();
         }
 
 
