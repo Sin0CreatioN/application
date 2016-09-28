ea6a082a681ed937558b21d298b197467ed5bf1c
anddev68 <chk392013@gmail.com>
2016-09-28 17:21:26 +0900

    Utilを変更

--- Question/app/src/main/java/tsmcomp/question/Util.java
+++ Question/app/src/main/java/tsmcomp/question/Util.java
@@ -9,9 +9,21 @@ import android.view.inputmethod.InputMethodManager;
  */
 public class Util {
 
+    /**
+     * キーボードを隠す処理
+     * @param context
+     * @param v
+     */
     public static void hideKeyboard(Context context, View v){
         v.requestFocus();
         InputMethodManager imm = (InputMethodManager)context.getSystemService(Context.INPUT_METHOD_SERVICE);
         imm.hideSoftInputFromWindow(v.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+    }
+
+    /**
+     * インターネット接続が有効かどうかを返す処理
+     */
+    public static boolean isConnectedInternet(){
+        return false;
     }
 }
