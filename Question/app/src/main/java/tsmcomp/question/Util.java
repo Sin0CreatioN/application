fe614b652b4f9aa0ae2b8cdc2626e1c3c0ed72b7
anddev68 <chk392013@gmail.com>
2016-09-28 17:22:58 +0900

    checking internet method was added

--- Question/app/src/main/java/tsmcomp/question/Util.java
+++ Question/app/src/main/java/tsmcomp/question/Util.java
@@ -1,6 +1,8 @@
 package tsmcomp.question;
 
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 
@@ -23,7 +25,13 @@ public class Util {
     /**
      * インターネット接続が有効かどうかを返す処理
      */
-    public static boolean isConnectedInternet(){
-        return false;
+    public static boolean isConnectedInternet(Context context){
+        ConnectivityManager cm =  (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo info = cm.getActiveNetworkInfo();
+        if( info != null ){
+            return info.isConnected();
+        } else {
+            return false;
+        }
     }
 }
